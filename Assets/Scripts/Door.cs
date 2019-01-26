44858f7623a9d3454a3e40f8d68c46b15ce0853d
Mike <mike@mike01.com>
2019-01-26 10:30:34 -0600

    Door script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Lock
+{
+    public bool opened;
+
+    public override bool Interact(HoldableObject carryingObject, PlayerInteraction playerInteraction)
+    {
+        if (locked)
+        {
+            if (base.Interact(carryingObject, playerInteraction))
+            {
+                Open();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        else
+        {
+            if (opened) {
+                Close();
+            } else
+            {
+                Open();
+            }
+            return true;
+        }
+    }
+
+    private void Open()
+    {
+        opened = true;
+        // call open animation
+    }
+
+    private void Close()
+    {
+        opened = false;
+        // call close  animation
+    }
+
+}
