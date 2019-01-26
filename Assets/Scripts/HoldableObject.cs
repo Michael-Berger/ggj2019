23d3a64072ad258507555cd3561be8dcf21337f8
David Bagwell <David@dbagwell.local>
2019-01-25 22:22:59 -0600

    - Added some base classes for objects.

---
+++ Assets/Scripts/HoldableObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldableObject : Interactable
+{
+
+    private bool canBePickedUp;
+    private bool isBeingHeld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canBePickedUp = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Highlight()
+    {
+        // Highlight the object to make it obvious that you can pick it up.
+    }
+
+    void Pickup()
+    {
+        canBePickedUp = false;
+        isBeingHeld = true;
+    }
+
+    void Drop()
+    {
+        canBePickedUp = true;
+        isBeingHeld = false;
+    }
+
+}
