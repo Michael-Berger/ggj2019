d5b31d6d638676d2142462a3b4744d0667f3107d
Mike <mike@mike01.com>
2019-01-26 15:26:31 -0600

    Keypad stuff

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : MonoBehaviour
+{
+    public delegate void LockHandler();
+
+    public event LockHandler Unlocked;
+    
+    public string keyCode;
+
+    private string currentSequence;
+
+    private bool locked = true;
+
+    private void Awake()
+    {
+        foreach (KeypadButton keypadButton in GetComponentsInChildren<KeypadButton>())
+        {
+            keypadButton.KeyPressed += ButtonPressed;
+        }
+    }
+
+
+    private void ButtonPressed(string key)
+    {
+        if (!locked)
+        {
+            return;
+        }
+
+        if (keyCode.Substring(currentSequence.Length, 1) == key)
+        {
+            currentSequence += key;
+            if (currentSequence == keyCode)
+            {
+                Unlocked?.Invoke();
+                locked = false;
+            }
+        }
+        else
+        {
+            currentSequence = "";
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/KeypadButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadButton : Interactable
+{
+
+    public delegate void KeyHandler(string code);
+    public event KeyHandler KeyPressed;
+
+    public string key;
+
+
+    public override bool Interact(HoldableObject carryingObject, PlayerInteraction playerInteraction)
+    {
+        if (carryingObject == null)
+        {
+            KeyPressed?.Invoke(key);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
