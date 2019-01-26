2906d390fa5009fadc5ad537c5bbdd7e586165d3
Taylor <taylor.eichhorst@gmail.com>
2019-01-26 13:15:14 -0600

    Lighting.  Doors also kind of work.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,6 +6,13 @@ public class Door : Lock
 {
     public bool opened;
 
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = transform.parent.GetComponent<Animator>();
+    }
+
     public override bool Interact(HoldableObject carryingObject, PlayerInteraction playerInteraction)
     {
         if (locked)
@@ -36,12 +43,14 @@ public class Door : Lock
     private void Open()
     {
         opened = true;
+        anim.SetTrigger("Open");
         // call open animation
     }
 
     private void Close()
     {
         opened = false;
+        anim.SetTrigger("Close");
         // call close  animation
     }
 
