f1a6f8f587373e14b01ca4a5dbad1c0537947aa8
David Bagwell <David@dbagwell.local>
2019-01-27 14:13:40 -0600

    - Added end trigger script. Needs to be added to mirror in storage room.

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+    public Mirror mirror;
+
+
+    private void Awake()
+    {
+
+        mirror.Teleported += TriggerEnd;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerEnd()
+    {
+        StartCoroutine(reloadScene());
+    }
+
+    private IEnumerator reloadScene()
+    {
+        yield return new WaitForSeconds(20);
+        SceneManager.LoadScene("House");
+    }
+}
