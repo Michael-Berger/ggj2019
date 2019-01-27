3155ca5b105bb2ec7d927926729ef7bf8f8a0ca3
David Bagwell <David@dbagwell.local>
2019-01-27 09:55:12 -0600

    - Turn fake TV on after delay when triggered. Adjustments to TV lighting.

---
+++ Assets/Scripts/MonitorScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorScreen : MonoBehaviour
+{
+
+    private Material material;
+    private new Light light;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        material.DisableKeyword("_EMISSION");
+        light = GetComponentInChildren<Light>();
+        light.enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowAfterDelay(float delay)
+    {
+        StartCoroutine(_ShowAfterDelay(delay));
+    }
+
+    private IEnumerator _ShowAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        material.EnableKeyword("_EMISSION");
+        light.enabled = true;
+    }
+}
